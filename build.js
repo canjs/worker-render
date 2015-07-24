9ff55888366ab032f88be41b9df776219c0bbd12
Matthew Phillips <matthew@matthewphillips.info>
2015-07-24 16:02:29 -0400

    Builds to every format now

--- build.js
+++ build.js
@@ -1,12 +1,29 @@
 var stealTools = require("steal-tools");
 
+var mains = [
+	"worker-render/window",
+	"worker-render/worker"
+];
+
 stealTools.export({
 	system: {
+		main: mains,
 		config: __dirname + "/package.json!npm"
 	},
 	outputs: {
-		"+cjs": {},
-		"+amd": {},
-		"+global-js": {}
+		"+cjs": {
+			graphs: mains
+		},
+		"+amd": {
+			graphs: mains
+		},
+		"+global-js window": {
+			modules: ["worker-render/window"],
+			dest: __dirname + "/dist/global/window.js"
+		},
+		"+global-js worker": {
+			modules: ["worker-render/worker"],
+			dest: __dirname + "/dist/global/worker.js"
+		}
 	}
 });
