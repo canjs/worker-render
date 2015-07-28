e90b071ba13cab3f13d8cf58faf4d78116ecc5b5
Matthew Phillips <matthew@matthewphillips.info>
2015-07-27 21:09:48 -0400

    the can monster

--- demo/basics/app.js
+++ demo/basics/app.js
@@ -4,6 +4,8 @@ importScripts("https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/unders
 importScripts("https://cdn.rawgit.com/jashkenas/dbmonster/gh-pages/junkola-copied-from-ember.js");
 importScripts("../../dist/global/vdom.js");
 importScripts("../../dist/global/worker.js");
+importScripts("../../node_modules/can/dist/can.jquery.js");
+importScripts("../../node_modules/can/dist/can.stache.js");
 
 workerRender.ready(render);
 
@@ -13,17 +15,55 @@ function render() {
 
 	var body = document.getElementsByTagName('body')[0];
 	var table;
+
+	var template = document.getElementById("template");
+	var renderer = can.stache(template.innerHTML.trim());
+
+	var list = new can.List(getDatabases());
+	body.appendChild(renderer({dbs: list}));
+
 	function redraw() {
 		var dbs = getDatabases();
 
-		var drawBefore = new Date();
-		newTable = template(dbs);
-		console.log("took", new Date() - drawBefore);
+		for(var i = 0, len = dbs.length; i < len; i++) {
+			var db = dbs[i];
+			var item = list.attr(i);
+
+			item.attr("queries").attr(db.queries, true);
+
+			/*var j = 0, jLen = db.queries.length, query;
+			for(; j < jLen; j++) {
+				query = item.attr("queries").attr(j);
+				if(!query) {
+					item.attr("queries").push(db.queries[i]);
+				} else {
+					query.attr(db.queries[j]);
+				}
+			}
+			jLen = item.attr("queries").attr("length");
+			for(; j < jLen; j++) {
+				item.attr("queries").pop();
+			}*/
+
+			j = 0;
+			jLen = 5;
+			for(; j < jLen; j++) {
+				item.attr("topFiveQueries").attr(j).attr(db.topFiveQueries[j]);
+			}
+
+			//item.attr("queries").replace(db.queries);
+			//item.attr("topFiveQueries").replace(db.topFiveQueries);
+		}
+
+		//list.replace(dbs);
+
+
+		/*newTable = template(dbs);
 		if(table) {
 			body.removeChild(table);
 		}
 		body.appendChild(newTable);
-		table = newTable;
+		table = newTable;*/
 		setTimeout(redraw, TIMEOUT);
 	}
 	redraw();
