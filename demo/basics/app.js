acee59113af34346b205ed05a1754936e9f36e78
Matthew Phillips <matthew@matthewphillips.info>
2015-07-30 20:16:37 -0400

    canjs dbmonster demo

--- demo/basics/app.js
+++ demo/basics/app.js
@@ -29,95 +29,19 @@ function render() {
 			var db = dbs[i];
 			var item = list.attr(i);
 
-			item.attr("queries").attr(db.queries, true);
-
-			/*var j = 0, jLen = db.queries.length, query;
-			for(; j < jLen; j++) {
-				query = item.attr("queries").attr(j);
-				if(!query) {
-					item.attr("queries").push(db.queries[i]);
-				} else {
-					query.attr(db.queries[j]);
-				}
-			}
-			jLen = item.attr("queries").attr("length");
-			for(; j < jLen; j++) {
-				item.attr("queries").pop();
-			}*/
+			item.attr({
+				countClassName: db.countClassName,
+				queryCount: db.queries.length
+			});
+			//item.attr("queries").attr(db.queries, true);
 
 			j = 0;
 			jLen = 5;
 			for(; j < jLen; j++) {
 				item.attr("topFiveQueries").attr(j).attr(db.topFiveQueries[j]);
 			}
-
-			//item.attr("queries").replace(db.queries);
-			//item.attr("topFiveQueries").replace(db.topFiveQueries);
 		}
-
-		//list.replace(dbs);
-
-
-		/*newTable = template(dbs);
-		if(table) {
-			body.removeChild(table);
-		}
-		body.appendChild(newTable);
-		table = newTable;*/
 		setTimeout(redraw, TIMEOUT);
 	}
 	redraw();
-}
-
-function template(dbs) {
-	// <table>
-	var table = document.createElement("table");
-	table.className = "table table-striped latest-data";
-
-	// <tbody>
-	var tbody = document.createElement("tbody");
-
-	var i = 0, len = dbs.length;
-	for(; i < len; i++) {
-		var db = dbs[i];
-		var tr = document.createElement("tr");
-
-		// <td dbname>
-		var dbname = document.createElement("td");
-		dbname.className = "dbname";
-		dbname.appendChild(document.createTextNode(db.name));
-		tr.appendChild(dbname);
-
-		// <td query-count>
-		var queryCount = document.createElement("td");
-		queryCount.className = "query-count";
-		var queryCountSpan = document.createElement("span");
-		queryCountSpan.className = "query-count";
-		queryCountSpan.appendChild(document.createTextNode(db.queries.length));
-		queryCount.appendChild(queryCountSpan);
-		tr.appendChild(queryCount);
-
-		// <td Query >
-		for(var j = 0, jLen = db.topFiveQueries.length; j < jLen; j++) {
-			var query = db.topFiveQueries[j];
-			var queryTd = document.createElement("td");
-			queryTd.appendChild(document.createTextNode(query.elapsed));
-
-			var popover = document.createElement("div");
-			popover.className = "popover left";
-			var popoverContent = document.createElement("div");
-			popoverContent.className = "popover-content";
-			popoverContent.appendChild(document.createTextNode(query.query));
-			popover.appendChild(popoverContent);
-			var arrow = document.createElement("div");
-			arrow.className = "arrow";
-			popover.appendChild(arrow);
-
-			queryTd.appendChild(popover);
-			tr.appendChild(queryTd);
-		}
-		tbody.appendChild(tr);
-	}
-	table.appendChild(tbody);
-	return table;
 }
