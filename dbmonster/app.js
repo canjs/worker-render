4916e094d4256a83613fe058ece2df531d1ace74
Matthew Phillips <matthew@matthewphillips.info>
2015-07-30 20:42:41 -0400

    Adding the dbmonster demo

---
+++ dbmonster/app.js
@@ -0,0 +1,46 @@
+window = self;
+
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js");
+importScripts("https://cdn.rawgit.com/jashkenas/dbmonster/gh-pages/junkola-copied-from-ember.js");
+importScripts("https://cdn.rawgit.com/canjs/worker-render/1.1.7/dist/global/vdom.js");
+importScripts("https://cdn.rawgit.com/canjs/worker-render/1.1.7/dist/global/worker.js");
+importScripts("https://cdn.rawgit.com/bitovi/canjs.com/v2.3.0-pre.1/can.jquery.js");
+importScripts("https://cdn.rawgit.com/bitovi/canjs.com/v2.3.0-pre.1/can.stache.js");
+
+workerRender.ready(render);
+
+function render() {
+	var TIMEOUT = 0;
+	ROWS = 100;
+
+	var body = document.getElementsByTagName('body')[0];
+	var table;
+
+	var template = document.getElementById("template");
+	var renderer = can.stache(template.innerHTML.trim());
+
+	var list = new can.List(getDatabases());
+	body.appendChild(renderer({dbs: list}));
+
+	function redraw() {
+		var dbs = getDatabases();
+
+		for(var i = 0, len = dbs.length; i < len; i++) {
+			var db = dbs[i];
+			var item = list.attr(i);
+
+			item.attr({
+				countClassName: db.countClassName,
+				queryCount: db.queries.length
+			});
+
+			j = 0;
+			jLen = 5;
+			for(; j < jLen; j++) {
+				item.attr("topFiveQueries").attr(j).attr(db.topFiveQueries[j]);
+			}
+		}
+		setTimeout(redraw, TIMEOUT);
+	}
+	redraw();
+}
